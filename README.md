a04f7f90f7fc4b54ed55d2d3ade5a50b49adc415
liang <zh_l2006@sina.com>
2019-02-24 12:20:59 -0700

    initial check in

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # randomness_test
+
+This is a repo for test of randomness.
+
+
+## Entropy measurement
+
+This could give one measurement of the randomness. We have normalized the entropy. So the value is always between 0 and 1. The larger the entropy is, the more random it is. 
+
+## How to run
+` cd entropy && node entropy.js`
+
+The output is :
+
+The input file length is : 15000
+The unique number count is: 13414
+The entropy is: 0.9843445072616206
+
+
---
+++ entropy/entropy.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+
+let buffer = fs.readFileSync("random.json");
+let input = JSON.parse(buffer.toString());
+let counts = {};
+
+console.log("The input file length is : " + input.length);
+for(let i = 0; i < input.length; ++i){
+    let num = input[i];
+    counts[num] = counts[num]? counts[num] +1 : 1;
+}
+
+let frequency = Object.keys(counts).map(key =>{
+    return counts[key];
+})
+
+let ps = frequency.map(fre => {
+    return fre/input.length;
+})
+
+console.log("The unique number count is: " + frequency.length);
+
+let entropy = 0;
+for(let i=0; i < ps.length; i++){
+    entropy = entropy - ps[i] * Math.log(ps[i]);
+}
+
+entropy = entropy/Math.log(input.length);
+
+console.log("The entropy is: " + entropy );
+
+
+
